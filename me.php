a4b4a2c578eecd6d82b585c5499ab1f887f69421
illia-stack <illiashapshalov38@gmail.com>
2026-07-20 07:49:15 +0200

    me

--- me.php
+++ me.php
@@ -1,7 +1,11 @@
 <?php
-    require_once __DIR__ . '/../includes/bootstrap.php';
 
-    echo json_encode([
-        "user" => $_SESSION['user'] ?? null
-    ]);
-?>
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once __DIR__ . "/includes/bootstrap.php";
+
+echo json_encode([
+    "user" => $_SESSION['user'] ?? null
+]);
